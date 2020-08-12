f864e75528ca5c0e3a3b801e1fad7f059f8347db
neven3 <61207254+neven3@users.noreply.github.com>
2020-08-12 05:30:58 +0200

    Updates

---
+++ precache-manifest.26e1066713ca072d3e2e72cae5e4fae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68b20b239646041b8b8f82c3240fdee",
+    "url": "/mono-project/index.html"
+  },
+  {
+    "revision": "16122beb907f0910e394",
+    "url": "/mono-project/static/css/main.87f236f1.chunk.css"
+  },
+  {
+    "revision": "943a4bf292298693a760",
+    "url": "/mono-project/static/js/2.3cc8d506.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mono-project/static/js/2.3cc8d506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16122beb907f0910e394",
+    "url": "/mono-project/static/js/main.302b033e.chunk.js"
+  },
+  {
+    "revision": "2eec541e3661c25d5693",
+    "url": "/mono-project/static/js/runtime-main.ef7a9e88.js"
+  }
+]);
