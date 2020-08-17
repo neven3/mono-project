c48520f8554e92e88947b5e4cf28d5040820c3ee
neven3 <61207254+neven3@users.noreply.github.com>
2020-08-17 06:47:30 +0200

    Tidy

--- src/Stores/PageStore.js
+++ src/Stores/PageStore.js
@@ -3,13 +3,12 @@ import {observable, action, decorate} from 'mobx';
 class PageStore {
     screen = 'home';
 
-    changeScreen = screen => this.screen = screen
+    changeScreen = screen => this.screen = screen;
 }
 
 decorate(PageStore, {
     screen: observable,
     changeScreen: action.bound,
-
 });
 
 const pageStore = new PageStore();
